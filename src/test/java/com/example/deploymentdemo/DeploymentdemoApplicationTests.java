5fe71c32ff56ba284ee5a6a95019534cb38dcd9b
JonasNu <99717692+JonasNu@users.noreply.github.com>
2024-05-14 08:21:16 +0200

    Add files via upload

---
+++ src/test/java/com/example/deploymentdemo/DeploymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
