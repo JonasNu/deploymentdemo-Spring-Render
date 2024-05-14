a2f94c6da7e87184ab5f62ffe31bccbf7102ac41
JonasNu <99717692+JonasNu@users.noreply.github.com>
2024-05-14 07:54:49 +0000

    test4

--- src/main/java/com/example/deploymentdemo/DeploymentdemoApplication.java
+++ src/main/java/com/example/deploymentdemo/DeploymentdemoApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
 public class DeploymentdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeploymentdemoApplication.class, args);
-	}
-
-	@GetMapping("/test")
-	public String getTest() {
-		return "success";
 	}
 }
