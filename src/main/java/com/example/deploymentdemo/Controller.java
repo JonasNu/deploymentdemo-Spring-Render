ff8fdc6a870cb75d33ba9bed91bd8d61cecb5cb7
JonasNu <99717692+JonasNu@users.noreply.github.com>
2024-05-27 13:47:15 +0000

    bugfix added test 2

--- src/main/java/com/example/deploymentdemo/Controller.java
+++ src/main/java/com/example/deploymentdemo/Controller.java
@@ -3,6 +3,7 @@ package com.example.deploymentdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.deploymentdemo.Results;
 
 @RestController
 @RequestMapping
